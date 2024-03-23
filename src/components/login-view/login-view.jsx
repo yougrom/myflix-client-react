d08295d730a96e77bdeebe94f18173b9ba949ce7
Herr Gromov <youriy.gromov@gmail.com>
2024-03-23 15:42:44 +0100

    Fixed localStorage saving to occur inside .then() after successful authentication.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -33,6 +33,8 @@ export const LoginView = ({ onLoggedIn }) => {
       })
       .then((data) => {
         if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
           onLoggedIn(data.user, data.token);
         } else {
           alert("No such user");
@@ -41,15 +43,8 @@ export const LoginView = ({ onLoggedIn }) => {
       .catch((e) => {
         alert("Something went wrong: " + e.message);
       });
-
-    if (data.user) {
-      localStorage.setItem("user", JSON.stringify(data.user));
-      localStorage.setItem("token", data.token);
-      onLoggedIn(data.user, data.token);
-    } else {
-      alert("No such user");
-    }
   };
+
   return (
     <Container>
       <Row>
